a9d2962eab179eedd99c8cf89ba7d25679bb4d58
eliasbokreta <elias.bokreta@gmail.com>
2021-06-04 18:35:34 +0200

    [Add] Locales files for enUS and frFR

---
+++ Locales/enUS.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("KillOnSight", "enUS", true)
+
+-- BG ZONES
+L["Warsong Gulch"] = "Warsong Gulch"
+L["Arathi Basin"] = "Arathi Basin"
+L["Alterac Valley"] = "Alterac Valley"
+L["Eye of the Storm"] = "Eye of the Storm"
+
+-- ARENA ZONES
+L["Nagrand Arena"] = "Nagrand Arena"
+L["Ruins of Lordaeron"] = "Ruins of Lordaeron"
+L["Dalaran Arena"] = "Dalaran Arena"
+L["The Ring of Valor"] = "The Ring of Valor"
---
+++ Locales/frFR.lua
@@ -0,0 +1,13 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("KillOnSight", "frFR")
+
+-- BG ZONES
+L["Warsong Gulch"] = "Goulet des Chanteguerres"
+L["Arathi Basin"] = "Bassin Arathi"
+L["Alterac Valley"] = "Vallée d’Alterac"
+L["Eye of the Storm"] = "L’Œil du cyclone"
+
+-- ARENA ZONES
+L["Nagrand Arena"] = "Arène de Nagrand"
+L["Ruins of Lordaeron"] = "Ruines de Lordaeron"
+L["Dalaran Arena"] = "Arène de Dalaran"
+L["The Ring of Valor"] = "L’arène des Valeureux"
