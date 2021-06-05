86cdcd62a492f1fef6e1274042647fc96c72df8e
eliasbokreta <elias.bokreta@gmail.com>
2021-06-06 00:07:59 +0200

    [Improvement] Scrolling tables structures stored in a separated file

---
+++ KillOnSightStructures.lua
@@ -0,0 +1,90 @@
+local addonName = "KillOnSight"
+local KillOnSight = LibStub("AceAddon-3.0"):GetAddon(addonName)
+
+KillOnSightListStructure = {
+    {
+        ["name"] = "Name",
+        ["width"] = 150,
+        ["align"] = "CENTER",
+        ["color"] = { 
+            ["r"] = 255.0, 
+            ["g"] = 255.0, 
+            ["b"] = 255.0, 
+            ["a"] = 1.0 
+        },
+        ["bgcolor"] = {
+            ["r"] = 0.0, 
+            ["g"] = 0.0, 
+            ["b"] = 255.0, 
+            ["a"] = 0.1
+        }
+    },
+    {
+        ["name"] = "Level",
+        ["width"] = 50,
+        ["align"] = "CENTER",
+        ["color"] = { 
+            ["r"] = 255.0, 
+            ["g"] = 255.0, 
+            ["b"] = 255.0, 
+            ["a"] = 1.0 
+        },
+        ["bgcolor"] = {
+            ["r"] = 0.0, 
+            ["g"] = 0.0, 
+            ["b"] = 255.0, 
+            ["a"] = 0.0
+        }
+    },
+    {
+        ["name"] = "Class",
+        ["width"] = 150,
+        ["align"] = "CENTER",
+        ["color"] = { 
+            ["r"] = 255.0, 
+            ["g"] = 255.0, 
+            ["b"] = 255.0, 
+            ["a"] = 1.0 
+        },
+        ["bgcolor"] = {
+            ["r"] = 0.0, 
+            ["g"] = 0.0, 
+            ["b"] = 255.0, 
+            ["a"] = 0.1
+        }
+    },
+    {
+        ["name"] = "Zone",
+        ["width"] = 150,
+        ["align"] = "CENTER",
+        ["color"] = { 
+            ["r"] = 255.0, 
+            ["g"] = 255.0, 
+            ["b"] = 255.0, 
+            ["a"] = 1.0 
+        },
+        ["bgcolor"] = {
+            ["r"] = 0.0, 
+            ["g"] = 0.0, 
+            ["b"] = 255.0, 
+            ["a"] = 0.0
+        }
+    },
+    {
+        ["name"] = "Date",
+        ["width"] = 150,
+        ["align"] = "CENTER",
+        ["color"] = { 
+            ["r"] = 255.0, 
+            ["g"] = 255.0, 
+            ["b"] = 255.0, 
+            ["a"] = 1.0 
+        },
+        ["bgcolor"] = {
+            ["r"] = 0.0, 
+            ["g"] = 0.0, 
+            ["b"] = 255.0, 
+            ["a"] = 0.1
+        }
+    },
+}
